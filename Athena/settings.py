fa259d2a151457840e467e12d5905357c3037fb8
ahmedjmal-tn <80910395+ahmedjmal-tn@users.noreply.github.com>
2023-05-30 16:11:16 +0100

    Update settings.py

--- Athena/settings.py
+++ Athena/settings.py
@@ -152,8 +152,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "Athena\static")]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Athena/static')]
+
 
 LOGIN_REDIRECT_URL = "/"
 
