321b22cab4cf5b45095098c2f00829132eb31491
ahmedjmal-tn <80910395+ahmedjmal-tn@users.noreply.github.com>
2023-05-30 16:20:01 +0100

    Update wsgi.py

--- Athena/wsgi.py
+++ Athena/wsgi.py
@@ -7,13 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Athena.settings')
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'Athena/static'))
 application.add_files(os.path.join(BASE_DIR, 'path/to/more/static/files'), prefix='more-files/')
+
 
