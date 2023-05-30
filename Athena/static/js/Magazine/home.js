06ec4a8ff8425b761341d00e1458d356a657f89d
ahmedjmal-tn <80910395+ahmedjmal-tn@users.noreply.github.com>
2023-05-30 15:17:58 +0100

    final

--- Athena/static/js/Magazine/home.js
+++ Athena/static/js/Magazine/home.js
@@ -49,52 +49,64 @@
 
  /* Mobile Menu
   * ---------------------------------------------------- */ 
-  const ssMobileMenu = function() {
-
-      const $navWrap = $('.s-header__nav-wrap');
-      const $closeNavWrap = $navWrap.find('.s-header__overlay-close');
-      const $menuToggle = $('.s-header__toggle-menu');
-      const $siteBody = $('body');
+ const ssMobileMenu = function() {
+    const $navWrap = $('.s-header__nav-wrap');
+    const $closeNavWrap = $navWrap.find('.s-header__overlay-close');
+    const $menuToggle = $('.s-header__toggle-menu');
+    const $siteBody = $('body');
+    let previousScroll = 0; // Store the previous scroll position
+  
+    $menuToggle.on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+  
+      $siteBody.addClass('nav-wrap-is-visible');
+    });
+  
+    $closeNavWrap.on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+  
+      if ($siteBody.hasClass('nav-wrap-is-visible')) {
+        $siteBody.removeClass('nav-wrap-is-visible');
+      }
+    });
+  
+    // Add the sticky class on scroll and hide/show the navigation bar
+    $(window).on('scroll', function() {
+      const currentScroll = $(window).scrollTop();
       
-      $menuToggle.on('click', function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-
-          $siteBody.addClass('nav-wrap-is-visible');
-      });
-
-      $closeNavWrap.on('click', function(e) {
-          e.preventDefault();
-          e.stopPropagation();
+      if (currentScroll > previousScroll) {
+        // Scrolling down
+        $('.s-header').removeClass('s-header--sticky').addClass('s-header--hide');
+      } else {
+        // Scrolling up
+        $('.s-header').addClass('s-header--sticky').removeClass('s-header--hide');
+      }
       
-          if($siteBody.hasClass('nav-wrap-is-visible')) {
-              $siteBody.removeClass('nav-wrap-is-visible');
-          }
-      });
-
-      // open (or close) submenu items in mobile view menu. 
-      // close all the other open submenu items.
-      $('.s-header__nav .has-children').children('a').on('click', function (e) {
-          e.preventDefault();
-
-          if ($(".close-mobile-menu").is(":visible") == true) {
-
-              $(this).toggleClass('sub-menu-is-open')
-                  .next('ul')
-                  .slideToggle(200)
-                  .end()
-                  .parent('.has-children')
-                  .siblings('.has-children')
-                  .children('a')
-                  .removeClass('sub-menu-is-open')
-                  .next('ul')
-                  .slideUp(200);
-
-          }
-      });
-
-  }; // end ssMobileMenu
-
+      previousScroll = currentScroll;
+    });
+  
+    // open (or close) submenu items in mobile view menu.
+    // close all the other open submenu items.
+    $('.s-header__nav .has-children').children('a').on('click', function(e) {
+      e.preventDefault();
+  
+      if ($('.close-mobile-menu').is(':visible') == true) {
+        $(this)
+          .toggleClass('sub-menu-is-open')
+          .next('ul')
+          .slideToggle(200)
+          .end()
+          .parent('.has-children')
+          .siblings('.has-children')
+          .children('a')
+          .removeClass('sub-menu-is-open')
+          .next('ul')
+          .slideUp(200);
+      }
+    });
+  };
 
  /* Search
   * ------------------------------------------------------ */
